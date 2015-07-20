58cc9ab32812140e14bbdc1fe4ea5367f32b0a55
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-07-20 15:05:50 -0700

    [issue550] add JsonPrinter, behaves like SmacPrinter

---
+++ src/search/docs/json_printer.cc
@@ -0,0 +1,232 @@
+#include "json_printer.h"
+
+using namespace std;
+
+namespace docs {
+
+JsonPrinter::JsonPrinter(ostream &out)
+    : DocPrinter(out) {
+}
+
+void JsonPrinter::print_synopsis(const DocStruct &info) {
+    if (!info.full_name.empty())
+        os << "# " << info.full_name << endl;
+}
+
+static string expand_infinity(const string &value) {
+    const int smac_infinity = numeric_limits<int>::max();
+    stringstream ss;
+    if (!value.compare("infinity")) {
+        ss << smac_infinity;
+    } else {
+        ss << value;
+    }
+    return ss.str();
+}
+
+static string get_upper_bound(const string &value) {
+    if (!value.compare("infinity")) {
+        return expand_infinity(value);
+    } else {
+        return "TODO";
+    }
+}
+
+string JsonPrinter::get_heuristic(const string &nick) const {
+    return "heuristic" + separator + nick;
+}
+
+string JsonPrinter::get_category(const string &type) const {
+    if (type == "Heuristic") {
+        return "heuristic";
+    } else if (type == "Labels") {
+        return "label_reduction";
+    } else if (type == "LandmarkGraph") {
+        return "landmarks";
+    } else if (type == "MergeStrategy") {
+        return "merge_strategy";
+    } else if (type == "OpenList") {
+        return "openlist";
+    } else if (type == "SearchEngine") {
+        return "search";
+    } else if (type == "ShrinkStrategy") {
+        return "shrink_strategy";
+    } else if (type == "Synergy") {
+        return "synergy"; // TODO: Return "heuristic"?
+    } else {
+        ABORT("Unknown type: " + type);
+    }
+}
+
+void JsonPrinter::print_parameter(const string &parameter, const string &feature, const ArgumentInfo &arg) {
+    string type;
+    string domain;
+    // TODO: We assume all integer and double params are >= 0.
+    if (!arg.type_name.compare("int")) {
+        type = "integer";
+        stringstream ss;
+        ss << "[0, " << get_upper_bound(arg.default_value) << "]";
+        domain = ss.str();
+    } else if (!arg.type_name.compare("double")) {
+        type = "real";
+        stringstream ss;
+        ss << "[0.0, " << get_upper_bound(arg.default_value) << "]";
+        domain = ss.str();
+    } else if (!arg.type_name.compare("bool")) {
+        type = "categorical";
+        domain = "{false, true}";
+    } else if (!arg.type_name.compare(0, 1, "{")) {
+        type = "categorical";
+        // Keep curly braces.
+        domain = lowercase(arg.type_name);
+    } else {
+        cerr << "Unrecognized type: " << arg.type_name << endl;
+        return;
+    }
+
+    os << parameter << " " << type << " " << domain << " ["
+       << lowercase(expand_infinity(arg.default_value))
+       << "]" << endl;
+    print_condition(parameter, feature);
+}
+
+void JsonPrinter::print_condition(const string &child,
+                                  const string &parent,
+                                  string condition) const {
+    if (condition.empty())
+        condition = parent + " != " + off;
+    os << child << " | " << condition << endl;
+}
+
+void JsonPrinter::print_helper_parameter(
+    const string &parent, const string& child, const string &type,
+    const string &range, const string &default_value, const string &condition) const {
+    string param = parent + separator + helper + child;
+    os << param << " " << type << " " << range << " [" << default_value << "]" << endl;
+    print_condition(param, parent, condition);
+}
+
+void JsonPrinter::print_weight(const string &parent, bool mixed) const {
+    string weight_param = parent + separator + helper + "weight";
+    os << weight_param << " ";
+    if (mixed) {
+        os << "real [0.1, 20] [1]";
+    } else {
+        os << "integer [1, 10] [1]";
+    }
+    os << endl;
+    print_condition(weight_param, parent);
+}
+
+void JsonPrinter::print_heuristic_helper_parameters(const string &heuristic_parameter) const {
+    // Add heuristic to search engine's preferred list.
+    string preferred_param = heuristic_parameter + separator + helper + "preferred";
+    print_bool(preferred_param);
+    print_condition(preferred_param, heuristic_parameter);
+
+    // Use heuristic for (weighted) estimates.
+    string single_param = heuristic_parameter + separator + helper + "single";
+    os << single_param << " categorical " << open_list_options << " [both]" << endl;
+    print_condition(single_param, heuristic_parameter);
+    print_weight(single_param, false);
+
+    // Use heuristic for single(sum(weight(g(), gw), weight(H, w)) open lists.
+    string sum_param = heuristic_parameter + separator + helper + "sum";
+    os << sum_param << " categorical " << open_list_options << " [" << off << "]" << endl;
+    print_condition(sum_param, heuristic_parameter);
+    print_weight(sum_param, true);
+
+    // Use heuristic for tiebreaking([sum(weight(g(), gw), weight(H, w)), H]) open lists.
+    string tb_param = heuristic_parameter + separator + helper + "tiebreaking";
+    os << tb_param << " categorical " << open_list_options << " [" << off << "]" << endl;
+    print_condition(tb_param, heuristic_parameter);
+    print_weight(tb_param, true);
+    string tb_on_h_param = tb_param + separator + helper + "on_h";
+    print_bool(tb_on_h_param);
+    print_condition(tb_on_h_param, tb_param);
+
+    // Use heuristic in linear combination or pareto open list.
+    for (string &open_list : vector<string>({lc, "pareto"})) {
+        string use_in_open_list_param = heuristic_parameter + separator + helper + open_list;
+        print_bool(use_in_open_list_param);
+        print_condition(use_in_open_list_param, heuristic_parameter);
+
+        print_weight(use_in_open_list_param, false);
+    }
+}
+
+void JsonPrinter::print_bool(const string &parameter) const {
+    os << parameter << " categorical " << bool_range << " [" << off << "]" << endl;
+}
+
+void JsonPrinter::print_usage(string plugin, const DocStruct &info) {
+    if (plugin.empty() || info.type == "AbstractTask" || info.type == "ScalarEvaluator")
+        return;
+    string feature = get_category(info.type) + separator + plugin;
+    if (info.type == "Heuristic") {
+        print_bool(feature);
+        print_heuristic_helper_parameters(feature);
+    } else if (info.type == "OpenList") {
+        // We add parameters for choosing open lists elsewhere.
+    } else if (info.type == "SearchEngine") {
+        print_bool(feature);
+    } else if (info.type == "LandmarkGraph") {
+        print_bool(feature);
+        print_condition(feature, get_heuristic("lmcount"));
+    } else if (info.type == "Synergy") {
+        print_bool(feature);
+        print_condition(feature, "",
+            get_heuristic("ff") + " == " + on + " && " +
+            get_heuristic("lmcount") + " == " + on);
+    }
+
+    for (const ArgumentInfo &arg : info.arg_help) {
+        if (arg.default_value == "<none>") {
+            if (arg.type_name.compare("AbstractTask"))
+                cerr << "Optional: " << arg.type_name << endl;
+            continue;
+        }
+        string parameter = feature + separator + arg.kwd;
+        print_parameter(parameter, feature, arg);
+    }
+}
+
+void JsonPrinter::print_arguments(const DocStruct &) {
+}
+
+void JsonPrinter::print_notes(const DocStruct &) {
+}
+
+void JsonPrinter::print_language_features(const DocStruct &) {
+}
+
+void JsonPrinter::print_properties(const DocStruct &) {
+}
+
+void JsonPrinter::print_category_header(string category_name) {
+    os << "# Parameters for " << category_name << endl << endl;
+}
+
+void JsonPrinter::print_category_footer() {
+    os << endl;
+}
+
+void JsonPrinter::print_all() {
+    // Additional open lists.
+    vector<string> open_lists = {lc, "pareto"};
+
+    for (auto &open_list : open_lists) {
+        string open_list_param = "openlist" + separator + helper + open_list;
+        os << open_list_param << " " << open_list_options << " [" << off << "]" << endl;
+
+        string open_list_g = open_list_param + separator + helper + "g";
+        print_bool(open_list_g);
+        print_condition(open_list_g, open_list_param);
+
+        print_weight(open_list_g, false);
+        os << endl;
+    }
+    DocPrinter::print_all();
+}
+
+}
---
+++ src/search/docs/json_printer.h
@@ -0,0 +1,50 @@
+#ifndef DOCS_JSON_PRINTER_H
+#define DOCS_JSON_PRINTER_H
+
+#include "../option_parser_util.h"
+
+namespace docs {
+
+class JsonPrinter : public DocPrinter {
+    const std::string separator = ":";
+    const std::string helper = "+";
+    const std::string lc = "linear_combination";
+    const std::string on = "true";
+    const std::string off = "false";
+    const std::string open_list_options = "{" + off + ", all_ops, pref_ops, both}";
+    const std::string bool_range = "{" + off + ", " + on + "}";
+
+    std::string get_category(const std::string &type) const;
+    std::string get_heuristic(const std::string &nick) const;
+    void print_bool(const std::string &parameter) const;
+    void print_parameter(const std::string &parameter, const std::string &feature, const ArgumentInfo &arg);
+    void print_condition(const std::string &child,
+                         const std::string &parent,
+                         std::string condition = "") const;
+    void print_weight(const std::string &parent, bool mixed) const;
+    void print_helper_parameter(
+        const std::string &parent, const std::string &child,
+        const std::string &type, const std::string &range,
+        const std::string &default_value, const std::string &condition) const;
+    void print_heuristic_helper_parameters(const std::string &heuristic_parameter) const;
+
+protected:
+    virtual void print_synopsis(const DocStruct &info);
+    virtual void print_usage(std::string plugin, const DocStruct &info);
+    virtual void print_arguments(const DocStruct &info);
+    virtual void print_notes(const DocStruct &info);
+    virtual void print_language_features(const DocStruct &info);
+    virtual void print_properties(const DocStruct &info);
+    virtual void print_category_header(std::string category_name);
+    virtual void print_category_footer();
+
+    virtual void print_all() override;
+
+public:
+    JsonPrinter(std::ostream &out);
+    virtual ~JsonPrinter() override = default;
+};
+
+}
+
+#endif
