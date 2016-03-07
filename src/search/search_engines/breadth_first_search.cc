b134df91d644b86e8870fae6a31b7ea3bf89920e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-03-07 13:48:39 +0100

    [unsolvable-search] remove unneeded members

--- src/search/search_engines/breadth_first_search.cc
+++ src/search/search_engines/breadth_first_search.cc
@@ -22,38 +22,18 @@ using namespace std;
 namespace breadth_first_search {
 BreadthFirstSearch::BreadthFirstSearch(const Options &opts)
     : SearchEngine(opts),
-      reopen_closed_nodes(opts.get<bool>("reopen_closed")),
-      use_multi_path_dependence(opts.get<bool>("mpd")),
       open_list(opts.get<shared_ptr<OpenListFactory>>("open")->
                 create_state_open_list()),
-      f_evaluator(opts.get<ScalarEvaluator *>("f_eval", nullptr)),
-      preferred_operator_heuristics(opts.get_list<Heuristic *>("preferred")),
       pruning_method(opts.get<shared_ptr<PruningMethod>>("pruning")) {
 }
 
 void BreadthFirstSearch::initialize() {
-    cout << "Conducting best first search"
-         << (reopen_closed_nodes ? " with" : " without")
-         << " reopening closed nodes, (real) bound = " << bound
+    cout << "Conducting best first search, (real) bound = " << bound
          << endl;
-    if (use_multi_path_dependence)
-        cout << "Using multi-path dependence (LM-A*)" << endl;
     assert(open_list);
 
     set<Heuristic *> hset;
     open_list->get_involved_heuristics(hset);
-
-    // add heuristics that are used for preferred operators (in case they are
-    // not also used in the open list)
-    hset.insert(preferred_operator_heuristics.begin(),
-                preferred_operator_heuristics.end());
-
-    // add heuristics that are used in the f_evaluator. They are usually also
-    // used in the open list and hence already be included, but we want to be
-    // sure.
-    if (f_evaluator) {
-        f_evaluator->get_involved_heuristics(hset);
-    }
 
     heuristics.assign(hset.begin(), hset.end());
     assert(!heuristics.empty());
@@ -70,7 +50,6 @@ void BreadthFirstSearch::initialize() {
     } else {
         if (search_progress.check_progress(eval_context))
             print_checkpoint_line(0);
-        start_f_value_statistics(eval_context);
         SearchNode node = search_space.get_node(initial_state);
         node.open_initial();
 
@@ -104,29 +83,9 @@ SearchStatus BreadthFirstSearch::step() {
         return SOLVED;
 
     vector<const GlobalOperator *> applicable_ops;
-    set<const GlobalOperator *> preferred_ops;
-
     g_successor_generator->generate_applicable_ops(s, applicable_ops);
 
-    /*
-      TODO: When preferred operators are in use, a preferred operator will be
-      considered by the preferred operator queues even when it is pruned.
-    */
     pruning_method->prune_operators(s, applicable_ops);
-
-    // This evaluates the expanded state (again) to get preferred ops
-    EvaluationContext eval_context(s, node.get_g(), false, &statistics, true);
-    for (Heuristic *heur : preferred_operator_heuristics) {
-        /* In an alternation search with unreliable heuristics, it is
-           possible that this heuristic considers the state a dead
-           end. We only want to ask for preferred operators for
-           finite-value heuristics. */
-        if (!eval_context.is_heuristic_infinite(heur)) {
-            vector<const GlobalOperator *> preferred =
-                eval_context.get_preferred_operators(heur);
-            preferred_ops.insert(preferred.begin(), preferred.end());
-        }
-    }
 
     for (const GlobalOperator *op : applicable_ops) {
         if ((node.get_real_g() + op->get_cost()) >= bound)
@@ -134,7 +93,6 @@ SearchStatus BreadthFirstSearch::step() {
 
         GlobalState succ_state = g_state_registry->get_successor_state(s, *op);
         statistics.inc_generated();
-        bool is_preferred = (preferred_ops.find(op) != preferred_ops.end());
 
         SearchNode succ_node = search_space.get_node(succ_state);
 
@@ -142,8 +100,10 @@ SearchStatus BreadthFirstSearch::step() {
         if (succ_node.is_dead_end())
             continue;
 
-        // update new path
-        if (use_multi_path_dependence || succ_node.is_new()) {
+        if (succ_node.is_new()) {
+            // We have not seen this state before.
+            // Evaluate and create a new node.
+
             /*
               Note: we must call reach_state for each heuristic, so
               don't break out of the for loop early.
@@ -151,11 +111,6 @@ SearchStatus BreadthFirstSearch::step() {
             for (Heuristic *heuristic : heuristics) {
                 heuristic->reach_state(s, *op, succ_state);
             }
-        }
-
-        if (succ_node.is_new()) {
-            // We have not seen this state before.
-            // Evaluate and create a new node.
 
             // Careful: succ_node.get_g() is not available here yet,
             // hence the stupid computation of succ_g.
@@ -163,7 +118,7 @@ SearchStatus BreadthFirstSearch::step() {
             int succ_g = node.get_g() + get_adjusted_cost(*op);
 
             EvaluationContext eval_context(
-                succ_state, succ_g, is_preferred, &statistics);
+                succ_state, succ_g, false, &statistics);
             statistics.inc_evaluated_states();
 
             if (open_list->is_dead_end(eval_context)) {
@@ -176,65 +131,13 @@ SearchStatus BreadthFirstSearch::step() {
             open_list->insert(eval_context, succ_state.get_id());
             if (search_progress.check_progress(eval_context)) {
                 print_checkpoint_line(succ_node.get_g());
-                reward_progress();
-            }
-        } else if (succ_node.get_g() > node.get_g() + get_adjusted_cost(*op)) {
-            // We found a new cheapest path to an open or closed state.
-            if (reopen_closed_nodes) {
-                if (succ_node.is_closed()) {
-                    /*
-                      TODO: It would be nice if we had a way to test
-                      that reopening is expected behaviour, i.e., exit
-                      with an error when this is something where
-                      reopening should not occur (e.g. A* with a
-                      consistent heuristic).
-                    */
-                    statistics.inc_reopened();
-                }
-                succ_node.reopen(node, op);
-
-                EvaluationContext eval_context(
-                    succ_state, succ_node.get_g(), is_preferred, &statistics);
-
-                /*
-                  Note: our old code used to retrieve the h value from
-                  the search node here. Our new code recomputes it as
-                  necessary, thus avoiding the incredible ugliness of
-                  the old "set_evaluator_value" approach, which also
-                  did not generalize properly to settings with more
-                  than one heuristic.
-
-                  Reopening should not happen all that frequently, so
-                  the performance impact of this is hopefully not that
-                  large. In the medium term, we want the heuristics to
-                  remember heuristic values for states themselves if
-                  desired by the user, so that such recomputations
-                  will just involve a look-up by the Heuristic object
-                  rather than a recomputation of the heuristic value
-                  from scratch.
-                */
-                open_list->insert(eval_context, succ_state.get_id());
-            } else {
-                // If we do not reopen closed nodes, we just update the parent pointers.
-                // Note that this could cause an incompatibility between
-                // the g-value and the actual path that is traced back.
-                succ_node.update_parent(node, op);
             }
         }
     }
-
     return IN_PROGRESS;
 }
 
 pair<SearchNode, bool> BreadthFirstSearch::fetch_next_node() {
-    /* TODO: The bulk of this code deals with multi-path dependence,
-       which is a bit unfortunate since that is a special case that
-       makes the common case look more complicated than it would need
-       to be. We could refactor this by implementing multi-path
-       dependence as a separate search algorithm that wraps the "usual"
-       search algorithm and adds the extra processing in the desired
-       places. I think this would lead to much cleaner code. */
-
     while (true) {
         if (open_list->empty()) {
             cout << "Completely explored state space -- no solution!" << endl;
@@ -242,9 +145,7 @@ pair<SearchNode, bool> BreadthFirstSearch::fetch_next_node() {
             SearchNode dummy_node = search_space.get_node(g_initial_state());
             return make_pair(dummy_node, false);
         }
-        vector<int> last_key_removed;
-        StateID id = open_list->remove_min(
-            use_multi_path_dependence ? &last_key_removed : nullptr);
+        StateID id = open_list->remove_min();
         // TODO is there a way we can avoid creating the state here and then
         //      recreate it outside of this function with node.get_state()?
         //      One way would be to store GlobalState objects inside SearchNodes
@@ -255,71 +156,20 @@ pair<SearchNode, bool> BreadthFirstSearch::fetch_next_node() {
         if (node.is_closed())
             continue;
 
-        if (use_multi_path_dependence) {
-            assert(last_key_removed.size() == 2);
-            if (node.is_dead_end())
-                continue;
-            int pushed_h = last_key_removed[1];
-
-            if (!node.is_closed()) {
-                EvaluationContext eval_context(
-                    node.get_state(), node.get_g(), false, &statistics);
-
-                if (open_list->is_dead_end(eval_context)) {
-                    node.mark_as_dead_end();
-                    statistics.inc_dead_ends();
-                    continue;
-                }
-                if (pushed_h < eval_context.get_result(heuristics[0]).get_h_value()) {
-                    assert(node.is_open());
-                    open_list->insert(eval_context, node.get_state_id());
-                    continue;
-                }
-            }
-        }
-
         node.close();
         assert(!node.is_dead_end());
-        update_f_value_statistics(node);
         statistics.inc_expanded();
         return make_pair(node, true);
     }
-}
-
-void BreadthFirstSearch::reward_progress() {
-    // Boost the "preferred operator" open lists somewhat whenever
-    // one of the heuristics finds a state with a new best h value.
-    open_list->boost_preferred();
 }
 
 void BreadthFirstSearch::dump_search_space() const {
     search_space.dump();
 }
 
-void BreadthFirstSearch::start_f_value_statistics(EvaluationContext &eval_context) {
-    if (f_evaluator) {
-        int f_value = eval_context.get_heuristic_value(f_evaluator);
-        statistics.report_f_value_progress(f_value);
-    }
-}
-
-/* TODO: HACK! This is very inefficient for simply looking up an h value.
-   Also, if h values are not saved it would recompute h for each and every state. */
-void BreadthFirstSearch::update_f_value_statistics(const SearchNode &node) {
-    if (f_evaluator) {
-        /*
-          TODO: This code doesn't fit the idea of supporting
-          an arbitrary f evaluator.
-        */
-        EvaluationContext eval_context(node.get_state(), node.get_g(), false, &statistics);
-        int f_value = eval_context.get_heuristic_value(f_evaluator);
-        statistics.report_f_value_progress(f_value);
-    }
-}
-
 /* TODO: merge this into SearchEngine::add_options_to_parser when all search
          engines support pruning. */
-void add_pruning_option(OptionParser &parser) {
+static void add_pruning_option(OptionParser &parser) {
     parser.add_option<shared_ptr<PruningMethod>>(
         "pruning",
         "Pruning methods can prune or reorder the set of applicable operators in "
@@ -332,28 +182,15 @@ static SearchEngine *_parse(OptionParser &parser) {
     parser.document_synopsis("Breadth-first search", "");
 
     parser.add_option<shared_ptr<OpenListFactory>>("open", "open list");
-    parser.add_option<bool>("reopen_closed",
-                            "reopen closed nodes", "false");
-    parser.add_option<ScalarEvaluator *>(
-        "f_eval",
-        "set evaluator for jump statistics. "
-        "(Optional; if no evaluator is used, jump statistics will not be displayed.)",
-        OptionParser::NONE);
-    parser.add_list_option<Heuristic *>(
-        "preferred",
-        "use preferred operators of these heuristics", "[]");
 
     add_pruning_option(parser);
     SearchEngine::add_options_to_parser(parser);
     Options opts = parser.parse();
 
-    BreadthFirstSearch *engine = nullptr;
-    if (!parser.dry_run()) {
-        opts.set<bool>("mpd", false);
-        engine = new BreadthFirstSearch(opts);
+    if (parser.dry_run()) {
+        return nullptr;
     }
-
-    return engine;
+    return new BreadthFirstSearch(opts);
 }
 
 static Plugin<SearchEngine> _plugin("breadth_first", _parse);
--- src/search/search_engines/breadth_first_search.h
+++ src/search/search_engines/breadth_first_search.h
@@ -19,21 +19,11 @@ class Options;
 
 namespace breadth_first_search {
 class BreadthFirstSearch : public SearchEngine {
-    const bool reopen_closed_nodes;
-    const bool use_multi_path_dependence;
-
     std::unique_ptr<StateOpenList> open_list;
-    ScalarEvaluator *f_evaluator;
-
     std::vector<Heuristic *> heuristics;
-    std::vector<Heuristic *> preferred_operator_heuristics;
-
     std::shared_ptr<PruningMethod> pruning_method;
 
     std::pair<SearchNode, bool> fetch_next_node();
-    void start_f_value_statistics(EvaluationContext &eval_context);
-    void update_f_value_statistics(const SearchNode &node);
-    void reward_progress();
     void print_checkpoint_line(int g) const;
 
 protected:
