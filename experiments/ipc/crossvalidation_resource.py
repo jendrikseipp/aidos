536b71e9e3ac18c9e7c77f9ab16217411a13bf89
Florian Pommerening <florian.pommerening@unibas.ch>
2016-03-30 13:32:06 +0200

    [unsolvable] fix random seed parameter in experiment

--- experiments/ipc/crossvalidation_resource.py
+++ experiments/ipc/crossvalidation_resource.py
@@ -11,7 +11,7 @@ import os
 configs = [
     IssueConfig(
         'rcp_nomystery_m07',
-        ['--random-seed 6753672',
+        ['--random-seed', '6753672',
          '--heuristic h_cpdbs=cpdbs(cache_estimates=false, cost_type=normal, dominance_pruning=false, patterns=combo(max_states=1))',
          '--heuristic h_deadpdbs_simple=deadpdbs_simple(patterns=combo(max_states=1), cost_type=normal, cache_estimates=false)',
          '--heuristic h_operatorcounting=operatorcounting(cache_estimates=false, constraint_generators=[feature_constraints(max_size=2), lmcut_constraints(), state_equation_constraints()], cost_type=normal)',
@@ -19,7 +19,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=true, pruning=null(), eval=max([h_cpdbs, h_deadpdbs_simple, h_operatorcounting, h_unsolvable_all_states_potential]))']),
     IssueConfig(
         'rcp_nomystery_m08_m09',
-        ['--random-seed 8529744',
+        ['--random-seed', '8529744',
          '--heuristic h_all_states_potential=all_states_potential(max_potential=1e8, cache_estimates=false, cost_type=normal)',
          '--heuristic h_cegar=cegar(subtasks=[goals(order=hadd_down), landmarks(order=original, combine_facts=true)], max_states=32342, use_general_costs=true, cost_type=normal, max_time=758.699959258, pick=max_refined, cache_estimates=false)',
          '--heuristic h_cpdbs=cpdbs(cache_estimates=true, cost_type=normal, dominance_pruning=true, patterns=systematic(only_interesting_patterns=true, pattern_max_size=1))',
@@ -32,7 +32,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=false, pruning=null(), eval=max([h_all_states_potential, h_cegar, h_cpdbs, h_deadpdbs, h_deadpdbs_simple, h_diverse_potentials, h_hmax, h_operatorcounting, h_unsolvable_all_states_potential]))']),
     IssueConfig(
         'rcp_rovers_m08_m09',
-        ['--random-seed 12444338',
+        ['--random-seed', '12444338',
          '--heuristic h_cegar=cegar(subtasks=[goals(order=hadd_down), landmarks(order=hadd_up, combine_facts=true), original(copies=1)], max_states=1199, use_general_costs=false, cost_type=normal, max_time=604.305232558, pick=min_unwanted, cache_estimates=false)',
          '--heuristic h_diverse_potentials=diverse_potentials(max_potential=1e8, cache_estimates=true, cost_type=normal, num_samples=362)',
          '--heuristic h_initial_state_potential=initial_state_potential(max_potential=1e8, cache_estimates=false, cost_type=normal)',
@@ -42,7 +42,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=false, pruning=null(), eval=max([h_cegar, h_diverse_potentials, h_initial_state_potential, h_sample_based_potentials, h_unsolvable_all_states_potential, h_zopdbs]))']),
     IssueConfig(
         'rcp_rovers_m05_m06',
-        ['--random-seed 13167049',
+        ['--random-seed', '13167049',
          '--heuristic h_blind=blind(cache_estimates=true, cost_type=normal)',
          '--heuristic h_deadpdbs=deadpdbs(cache_estimates=true, cost_type=normal, max_dead_ends=28832, max_time=1074.07153225, patterns=systematic(only_interesting_patterns=true, pattern_max_size=1))',
          '--heuristic h_deadpdbs_simple=deadpdbs_simple(patterns=systematic(only_interesting_patterns=true, pattern_max_size=1), cost_type=normal, cache_estimates=true)',
@@ -55,13 +55,13 @@ configs = [
          '--search astar(f_bound=compute, mpd=true, pruning=stubborn_sets_simple(min_pruning_ratio=0.788069573101691), eval=max([h_blind, h_deadpdbs, h_deadpdbs_simple, h_hmax, h_initial_state_potential, h_lmcut, h_operatorcounting, h_pdb, h_unsolvable_all_states_potential]))']),
     IssueConfig(
         'rcp_tpp_m05_m06',
-        ['--random-seed 6673498',
+        ['--random-seed', '6673498',
          '--heuristic h_hmax=hmax(cache_estimates=false, cost_type=normal)',
          '--heuristic h_lmcut=lmcut(cache_estimates=true, cost_type=normal)',
          '--search astar(f_bound=compute, mpd=false, pruning=null(), eval=max([h_hmax, h_lmcut]))']),
     IssueConfig(
         'rcp_tpp_m07',
-        ['--random-seed 9596183',
+        ['--random-seed', '9596183',
          '--heuristic h_deadpdbs=deadpdbs(cache_estimates=false, cost_type=normal, max_dead_ends=1326, max_time=1082.64300144, patterns=systematic(only_interesting_patterns=true, pattern_max_size=1))',
          '--heuristic h_deadpdbs_simple=deadpdbs_simple(patterns=systematic(only_interesting_patterns=true, pattern_max_size=1), cost_type=normal, cache_estimates=true)',
          '--heuristic h_diverse_potentials=diverse_potentials(max_potential=1e8, cache_estimates=true, cost_type=normal, num_samples=3)',
@@ -74,7 +74,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=false, pruning=stubborn_sets_simple(min_pruning_ratio=0.1406851010605542), eval=max([h_deadpdbs, h_deadpdbs_simple, h_diverse_potentials, h_initial_state_potential, h_lmcut, h_operatorcounting, h_pdb, h_unsolvable_all_states_potential, h_zopdbs]))']),
     IssueConfig(
         'rcp_nomystery_m05_m06',
-        ['--random-seed 4876933',
+        ['--random-seed', '4876933',
          '--heuristic h_initial_state_potential=initial_state_potential(max_potential=1e8, cache_estimates=false, cost_type=normal)',
          '--heuristic h_lmcut=lmcut(cache_estimates=true, cost_type=normal)',
          '--heuristic h_operatorcounting=operatorcounting(cache_estimates=false, cost_type=normal, constraint_generators=[pho_constraints(patterns=genetic(disjoint=true, mutation_probability=0.6926711167943063, pdb_max_size=497605, num_collections=4, num_episodes=6))])',
@@ -83,7 +83,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=true, pruning=stubborn_sets_simple(min_pruning_ratio=0.20943355142564968), eval=max([h_initial_state_potential, h_lmcut, h_operatorcounting, h_sample_based_potentials, h_unsolvable_all_states_potential]))']),
     IssueConfig(
         'rcp_rovers_m07',
-        ['--random-seed 1466751',
+        ['--random-seed', '1466751',
          '--heuristic h_blind=blind(cache_estimates=false, cost_type=normal)',
          '--heuristic h_cegar=cegar(subtasks=[original(copies=1)], max_states=91983, use_general_costs=false, cost_type=normal, max_time=911.55747802, pick=max_hadd, cache_estimates=true)',
          '--heuristic h_cpdbs=cpdbs(cache_estimates=true, cost_type=normal, dominance_pruning=false, patterns=systematic(only_interesting_patterns=true, pattern_max_size=1))',
@@ -93,7 +93,7 @@ configs = [
          '--search astar(f_bound=compute, mpd=true, pruning=stubborn_sets_ec(min_pruning_ratio=0.6604351517386684), eval=max([h_blind, h_cegar, h_cpdbs, h_deadpdbs, h_diverse_potentials, h_lmcut]))']),
     IssueConfig(
         'rcp_tpp_m08_m09',
-        ['--random-seed 5335524',
+        ['--random-seed', '5335524',
          '--heuristic h_all_states_potential=all_states_potential(max_potential=1e8, cache_estimates=false, cost_type=normal)',
          '--heuristic h_blind=blind(cache_estimates=true, cost_type=normal)',
          '--heuristic h_cegar=cegar(subtasks=[landmarks(order=random, combine_facts=true)], max_states=5096, use_general_costs=true, cost_type=normal, max_time=87.8020597541, pick=max_unwanted, cache_estimates=true)',
