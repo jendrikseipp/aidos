a44841ed1885e045f10ca9a4384d60bc05513ddc
Florian Pommerening <florian.pommerening@unibas.ch>
2016-03-31 17:05:12 +0200

    [unsolvable] filter unsolvable for resource configs

--- experiments/ipc/crossvalidation_all.py
+++ experiments/ipc/crossvalidation_all.py
@@ -322,6 +322,13 @@ exp.add_suite(get_domain_dir(), [
     "unsolvable-tetris",
 ])
 
-exp.add_absolute_report_step(attributes=exp.DEFAULT_TABLE_ATTRIBUTES + ["unsolvable"])
+def add_unsolvable(run):
+    if run.get("error") == "incomplete-search-found-no-plan":
+        run["unsolvable"] = 1
+    return run
+
+exp.add_absolute_report_step(
+    filter=add_unsolvable,
+    attributes=exp.DEFAULT_TABLE_ATTRIBUTES + ["unsolvable"])
 
 exp()
