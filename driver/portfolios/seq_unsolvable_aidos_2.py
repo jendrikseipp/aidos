e983030218f2be76703ac2b152c3ee855e60461c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-04-04 17:48:41 +0200

    [unsolvable] update portfolios

---
+++ driver/portfolios/seq_unsolvable_aidos_2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+OPTIMAL = True
+
+CONFIGS = [
+    (1, [
+        "--heuristic",
+        "h_seq=operatorcounting([state_equation_constraints(), feature_constraints(max_size=infinity)], cost_type=zero)",
+        "--search",
+        "unsolvable_search([h_seq], pruning=stubborn_sets_ec(min_pruning_ratio=0.20))"]),
+    (4, [
+        "--search",
+        "unsolvable_search([deadpdbs(max_time=relative time 1)], pruning=stubborn_sets_ec(min_pruning_ratio=0.80))"]),
+    (598, [
+        "--heuristic",
+        "h_seq=operatorcounting([state_equation_constraints(), lmcut_constraints()], cost_type=zero)",
+        "--heuristic",
+        "h_cegar=cegar(subtasks=[original()], pick=max_hadd, max_time=relative time 75)",
+        "--search",
+        "astar(f_bound=compute, eval=max([h_cegar, h_seq]), pruning=stubborn_sets_ec(min_pruning_ratio=0.50))"]),
+    (598, [
+        "--search",
+        "unsolvable_search([deadpdbs(max_time=relative time 50)], pruning=stubborn_sets_ec(min_pruning_ratio=0.80))"]),
+    (599, [
+        "--heuristic",
+        "h_seq=operatorcounting([state_equation_constraints(), feature_constraints(max_size=infinity)], cost_type=zero)",
+        "--search",
+        "unsolvable_search([h_seq], pruning=stubborn_sets_ec(min_pruning_ratio=0.20))"]),
+]
+
+assert sum(t for t, _ in CONFIGS) == 1800
