65cbfd44b2c6af513a9f70568121e1b9ff14f1f8
Jendrik Seipp <jendrik.seipp@liu.se>
2021-05-09 16:49:08 +0200

    [ci] Update default build type.

--- build_configs.py
+++ build_configs.py
@@ -1,8 +1,22 @@
-release32 = ["-DCMAKE_BUILD_TYPE=Release"]
-debug32 = ["-DCMAKE_BUILD_TYPE=Debug"]
-release64 = ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
-debug64 = ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
+release = ["-DCMAKE_BUILD_TYPE=Release"]
+debug = ["-DCMAKE_BUILD_TYPE=Debug"]
+# USE_GLIBCXX_DEBUG is not compatible with USE_LP (see issue983).
+glibcxx_debug = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
- 
-DEFAULT = "release32"
-DEBUG = "debug32"
+
+# Aidos 1 and 2 can use this minimal build. Aidos 3 needs "release".
+aidos = [
+    "-DCMAKE_BUILD_TYPE=Release",
+    "-DDISABLE_PLUGINS_BY_DEFAULT=YES",
+    "-DPLUGIN_CEGAR_ENABLED=YES",
+    "-DPLUGIN_EAGER_SEARCH_ENABLED=YES",
+    "-DPLUGIN_IPC_MAX_HEURISTIC_ENABLED=YES",
+    "-DPLUGIN_LANDMARK_CUT_HEURISTIC_ENABLED=YES",
+    "-DPLUGIN_OPERATOR_COUNTING_ENABLED=YES",
+    "-DPLUGIN_PDBS_ENABLED=YES",
+    "-DPLUGIN_StubbornSetsEC_ENABLED=YES",
+    "-DPLUGIN_UNSOLVABLE_SEARCH_ENABLED=YES",
+]
+
+DEFAULT = "release"
+DEBUG = "debug"
